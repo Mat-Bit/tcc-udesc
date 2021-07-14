f972c0115654b0ff5948976b964ff736487c8022
Mat-Bit <mateus.lbittencourt@gmail.com>
2021-07-14 01:48:47 -0300

    Adding Dropout

--- xception.py
+++ xception.py
@@ -5,6 +5,7 @@ import matplotlib
 import imghdr
 import pickle as pkl
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
@@ -12,12 +13,12 @@ from tensorflow.keras.applications.xception import Xception, preprocess_input
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.losses import binary_crossentropy
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import CSVLogger
-# from tensorflow.keras.metrics import AUC
+from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve
 
 
 matplotlib.use('Agg')
@@ -25,24 +26,34 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 parser = argparse.ArgumentParser()
 parser.add_argument('name')
 parser.add_argument('dataset_root')
-parser.add_argument('classes')
 parser.add_argument('result_root')
 parser.add_argument('--epochs_pre', type=int, default=10)
-parser.add_argument('--epochs_fine', type=int, default=50)
+parser.add_argument('--epochs_fine', type=int, default=40)
 parser.add_argument('--batch_size_pre', type=int, default=16)
 parser.add_argument('--batch_size_fine', type=int, default=32)
 parser.add_argument('--lr_pre', type=float, default=1e-3)
-parser.add_argument('--lr_fine', type=float, default=2e-4)
-parser.add_argument('--split', type=float, default=0.8)
+parser.add_argument('--lr_fine', type=float, default=4e-4)
+parser.add_argument('--test_split', type=float, default=0.1)
+parser.add_argument('--val_split', type=float, default=0.2)
+parser.add_argument('--dropout', type=float, default=0.5)
+
+
+METRICS = [
+    # tf.keras.metrics.TruePositives(name='tp'),
+    # tf.keras.metrics.FalsePositives(name='fp'),
+    # tf.keras.metrics.TrueNegatives(name='tn'),
+    # tf.keras.metrics.FalseNegatives(name='fn'), 
+    tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc')
+]
 
 
 def generate_from_paths_and_labels(
         input_paths, labels, batch_size, input_size=(299, 299)):
     num_samples = len(input_paths)
     while 1:
-        perm = np.random.permutation(num_samples)
-        input_paths = input_paths[perm]
-        labels = labels[perm]
         for i in range(0, num_samples, batch_size):
             inputs = list(map(
                 lambda x: image.load_img(x, target_size=input_size),
@@ -56,6 +67,44 @@ def generate_from_paths_and_labels(
             yield (inputs, labels[i:i+batch_size])
 
 
+def plot_metrics(history, name_path, title):
+    metrics = ['accuracy', 'loss', 'precision', 'recall']
+    plt.figure(figsize=(8,8))
+    plt.title(title)
+
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_"," ").capitalize()
+        plt.subplot(2,2,n+1)
+        plt.plot(history.epoch, history.history[metric], label='Train')
+        plt.plot(history.epoch, history.history['val_'+metric], linestyle="--", label='Val')
+        if n >= len(metrics) // 2:
+            plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, 6])
+        elif metric == 'auc':
+            plt.ylim([0.8,1])
+        else:
+            plt.ylim([0.3,1])
+
+        plt.legend()
+    plt.savefig(name_path + 'fit_metrics.png')
+    plt.clf()
+
+
+def plot_roc(name, labels, predictions, **kwargs):
+    fp, tp, _ = roc_curve(labels, predictions)
+
+    plt.plot(fp, tp, label=name, linewidth=2, **kwargs)
+    plt.xlabel('False positives [%]')
+    plt.ylabel('True positives [%]')
+    plt.xlim([-0.05,0.40])
+    plt.ylim([0.6,1.05])
+    plt.grid(True)
+    ax = plt.gca()
+    ax.set_aspect('equal')
+
+
 def main(args):
 
     # ====================================================
@@ -65,12 +114,35 @@ def main(args):
     epochs = args.epochs_pre + args.epochs_fine
     args.dataset_root = os.path.expanduser(args.dataset_root)
     args.result_root = os.path.expanduser(args.result_root)
-    args.classes = os.path.expanduser(args.classes)
 
-    # load class names
-    with open(args.classes, 'r') as f:
-        classes = f.readlines()
-        classes = list(map(lambda x: x.strip(), classes))
+    # create a root directory of results 
+    if os.path.exists(args.result_root) is False:
+        os.makedirs(args.result_root)
+    
+    # create a directory where results will be stored
+    result_path_name = os.path.join(args.result_root, args.name)
+    if os.path.exists(result_path_name) is False:
+        os.makedirs(result_path_name)
+
+    with open(f'{result_path_name}/parameters.txt', 'w') as file:
+        file.write(f"\tTeste: {args.name}\n\n")
+
+        file.write(f"Diretorio Dataset: {args.dataset_root};\n")
+        file.write(f"Diretorio dos Resultados: {result_path_name};\n\n")
+
+        file.write(f"Dropout Regularization: {args.dropout};\n\n")
+
+        file.write(f"\tPre-treino (apenas ultima camada):\n")
+        file.write(f"Epocas: {args.epochs_pre};\n")
+        file.write(f"Batch Size: {args.batch_size_pre};\n")
+        file.write(f"Learning Rate: {args.lr_pre};\n\n")
+
+        file.write(f"\tFine Tunning de toda Arquitetura:\n")
+        file.write(f"Epocas: {args.epochs_fine};\n")
+        file.write(f"Batch Size: {args.batch_size_fine};\n")
+        file.write(f"Learning Rate: {args.lr_fine};\n")
+
+    classes = os.listdir(args.dataset_root)
     num_classes = len(classes)
 
     # make input_paths and labels
@@ -92,25 +164,35 @@ def main(args):
     # convert to numpy array
     input_paths = np.array(input_paths)
 
-    # shuffle dataset
-    perm = np.random.permutation(len(input_paths))
-    labels = labels[perm]
-    input_paths = input_paths[perm]
+    # split dataset for training and test
+    train_input_paths, test_input_paths, train_labels, test_labels = train_test_split(input_paths, labels, test_size=args.test_split, shuffle=True, random_state=47)
 
-    print("Input_paths:", len(input_paths))
+    # split the training dataset for training and validation
+    train_input_paths, val_input_paths, train_labels, val_labels = train_test_split(train_input_paths, train_labels, test_size=args.val_split, shuffle=True, random_state=26)
 
-    # split dataset for training and validation
-    train_input_paths, val_input_paths, train_labels, val_labels = train_test_split(input_paths, labels, train_size=args.split)
-    
     print("Training on %d images and labels" % (len(train_input_paths)))
     print("Validation on %d images and labels" % (len(val_input_paths)))
+    print("Test on %d images and labels" % (len(test_input_paths)))
 
-    # create a directory where results will be saved (if necessary)
-    if os.path.exists(args.result_root) is False:
-        os.makedirs(args.result_root)
-    
+    df = pd.DataFrame(columns=['Image', 'Subset', 'Label'])
+
+    for i in range(len(train_input_paths)):
+        df.loc[i] = [train_input_paths[i], 'Train', train_labels[i]]
+
+    n = len(df)
+    for i in range(len(val_input_paths)):
+        df.loc[n+i] = [val_input_paths[i], 'Validation', val_labels[i]]
+
+    n = len(df)
+    for i in range(len(test_input_paths)):
+        df.loc[n+i] = [test_input_paths[i], 'Test', test_labels[i]]
+
+    dataset_desc = os.path.join(result_path_name, 'dataset_desc.csv')
+    df.to_csv(dataset_desc, index=False)
+
     # Create a CSV logger file
-    csv_logger = CSVLogger(f'{args.name}_log.csv', append=True)
+    csv_logger_file = os.path.join(result_path_name, 'training_log.csv')
+    csv_logger = CSVLogger(csv_logger_file, append=True)
 
     # ====================================================
     # Build a custom Xception
@@ -127,6 +209,7 @@ def main(args):
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     x = Dense(1024, activation='relu')(x)
+    x = Dropout(args.dropout, input_shape=(1024,))(x)
     predictions = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=base_model.inputs, outputs=predictions)
 
@@ -141,10 +224,9 @@ def main(args):
     model.compile(
         loss=binary_crossentropy,
         optimizer=Adam(lr=args.lr_pre),
-        metrics=['accuracy']
+        metrics=METRICS
     )
 
-    # Generate a train and validation sets for pre training
     train_dataset = generate_from_paths_and_labels(
         input_paths=train_input_paths,
         labels=train_labels,
@@ -169,7 +251,6 @@ def main(args):
         verbose=1,
         callbacks=[csv_logger]
     )
-    model.save(os.path.join(args.result_root, f'model_{args.name}_pre_training.h5'))
 
     # ====================================================
     # Train the whole model
@@ -182,7 +263,8 @@ def main(args):
     model.compile(
         loss=binary_crossentropy,
         optimizer=Adam(lr=args.lr_fine),
-        metrics=['accuracy'])
+        metrics=METRICS
+    )
 
     # train
     hist_fine = model.fit(
@@ -196,70 +278,39 @@ def main(args):
         verbose=1,
         callbacks=[csv_logger],
     )
-    model.save(os.path.join(args.result_root, f'model_{args.name}_fine_tunning.h5'))
+    model.save(os.path.join(result_path_name, f'model_trained.h5'))
 
     # ====================================================
     # Create & save result graphs
     # ====================================================
-    # concatinate plot data
-    acc = hist_pre.history['accuracy']
-    val_acc = hist_pre.history['val_accuracy']
-    loss = hist_pre.history['loss']
-    val_loss = hist_pre.history['val_loss']
-    acc.extend(hist_fine.history['accuracy'])
-    val_acc.extend(hist_fine.history['val_accuracy'])
-    loss.extend(hist_fine.history['loss'])
-    val_loss.extend(hist_fine.history['val_loss'])
 
-    # save graph image
-    plt.plot(range(epochs), acc, marker='.', label='accuracy')
-    plt.plot(range(epochs), val_acc, marker='.', label='val_accuracy')
-    plt.legend(loc='best')
-    plt.grid()
-    plt.xlabel('epoch')
-    plt.ylabel('accuracy')
-    plt.savefig(os.path.join(args.result_root, 'accuracy.png'))
+    # Plot the training metrics
+    plot_path = os.path.join(result_path_name, 'pre_training_')
+    plot_metrics(hist_fine, plot_path, 'Pre Treino')
+
+    plot_path = os.path.join(result_path_name, 'fine_tunning_')
+    plot_metrics(hist_fine, plot_path, 'Fine Tunning')
+    
+
+    test_dataset = generate_from_paths_and_labels(
+        input_paths=test_input_paths,
+        labels=test_labels,
+        batch_size=args.batch_size_pre
+    )
+
+    train_predictions_baseline = model.predict(train_dataset)
+    test_predictions_baseline = model.predict(test_dataset)
+
+    # Plot the ROC curve
+    plot_roc("Train", train_labels, train_predictions_baseline)
+    plot_roc("Test", test_labels, test_predictions_baseline)
+    plt.savefig(os.path.join(args.result_root, 'auroc.png'))
     plt.clf()
-
-    plt.plot(range(epochs), loss, marker='.', label='loss')
-    plt.plot(range(epochs), val_loss, marker='.', label='val_loss')
-    plt.legend(loc='best')
-    plt.grid()
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.savefig(os.path.join(args.result_root, 'loss.png'))
-    plt.clf()
-
-    # save plot data as pickle file
-    plot = {
-        'accuracy': acc,
-        'val_accuracy': val_acc,
-        'loss': loss,
-        'val_loss': val_loss,
-    }
-    with open(os.path.join(args.result_root, 'plot.dump'), 'wb') as f:
-        pkl.dump(plot, f)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
     print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
-
-    with open(f'{args.result_root}/{args.name}', 'w') as file:
-        file.write(f"\tTeste: {args.name}\n\n")
-
-        file.write(f"Diretorio Dataset: {args.dataset_root};\n")
-        file.write(f"Arquivo com as Classes: {args.classes};\n\n")
-
-        file.write(f"\tPre-treino (apenas ultima camada):\n")
-        file.write(f"Epocas: {args.epoch_pre};\n")
-        file.write(f"Batch Size: {args.batch_size_pre};\n")
-        file.write(f"Learning Rate: {args.lr_pre};\n")
-
-        file.write(f"\tFine Tunning de toda Arquitetura:\n")
-        file.write(f"Epocas: {args.epochs_fine};\n")
-        file.write(f"Batch Size: {args.batch_size_fine};\n")
-        file.write(f"Learning Rate: {args.lr_fine};\n")
 
     main(args)
