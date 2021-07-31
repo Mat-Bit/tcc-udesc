c3280e54101034baa9917231d9f738a983e61565
Mat-Bit <mateus.lbittencourt@gmail.com>
2021-07-31 00:50:50 -0300

    Improve Xcetion model file

--- xception.py
+++ xception.py
@@ -15,7 +15,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve
@@ -27,12 +27,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('name')
 parser.add_argument('dataset_root')
 parser.add_argument('result_root')
-parser.add_argument('--epochs_pre', type=int, default=15)
-parser.add_argument('--epochs_fine', type=int, default=70)
-parser.add_argument('--batch_size_pre', type=int, default=16)
+parser.add_argument('--epochs_pre', type=int, default=10)
+parser.add_argument('--epochs_fine', type=int, default=20)
+parser.add_argument('--batch_size_pre', type=int, default=32)
 parser.add_argument('--batch_size_fine', type=int, default=32)
 parser.add_argument('--lr_pre', type=float, default=1e-3)
-parser.add_argument('--lr_fine', type=float, default=2e-4)
+parser.add_argument('--lr_fine', type=float, default=5e-4)
 parser.add_argument('--test_split', type=float, default=0.1)
 parser.add_argument('--val_split', type=float, default=0.2)
 parser.add_argument('--dropout', type=float, default=0.5)
@@ -63,6 +63,23 @@ def generate_from_paths_and_labels(
             yield (inputs, labels[i:i+batch_size])
 
 
+def generate_from_paths(
+        input_paths, batch_size, input_size=(299, 299)):
+    num_samples = len(input_paths)
+    while 1:
+        for i in range(0, num_samples, batch_size):
+            inputs = list(map(
+                lambda x: image.load_img(x, target_size=input_size),
+                input_paths[i:i+batch_size]
+            ))
+            inputs = np.array(list(map(
+                lambda x: image.img_to_array(x),
+                inputs
+            )))
+            inputs = preprocess_input(inputs)
+            yield inputs
+
+
 def plot_metrics(history, name_path, title):
     metrics = ['accuracy', 'loss', 'precision', 'recall']
     plt.figure(figsize=(8,8))
@@ -72,16 +89,14 @@ def plot_metrics(history, name_path, title):
         name = metric.replace("_"," ").capitalize()
         plt.subplot(2,2,n+1)
         plt.plot(history.epoch, history.history[metric], label='Train')
-        plt.plot(history.epoch, history.history['val_'+metric], linestyle="--", label='Val')
+        plt.plot(history.epoch, history.history['val_'+ metric], linestyle="--", label='Val')
         if n >= len(metrics) // 2:
             plt.xlabel('Epoch')
         plt.ylabel(name)
         if metric == 'loss':
-            plt.ylim([0, 6])
-        elif metric == 'auc':
-            plt.ylim([0.8,1])
+            plt.ylim([0, 4])
         else:
-            plt.ylim([0.3,1])
+            plt.ylim([0.45,1.1])
 
         plt.legend()
     plt.savefig(name_path + 'fit_metrics.png')
@@ -143,7 +158,8 @@ def main(args):
 
     # make input_paths and labels
     input_paths, labels = [], []
-    for class_name in os.listdir(args.dataset_root):
+    classes_dir = [class_path for class_path in os.listdir(args.dataset_root) if os.path.isdir(os.path.join(args.dataset_root, class_path))]
+    for class_name in classes_dir:
         class_root = os.path.join(args.dataset_root, class_name)
         class_id = classes.index(class_name)
         for path in os.listdir(class_root):
@@ -190,6 +206,16 @@ def main(args):
     csv_logger_file = os.path.join(result_path_name, 'training_log.csv')
     csv_logger = CSVLogger(csv_logger_file, append=True)
 
+    # Create a callback to save the model weights
+    checkpoint_file = os.path.join(result_path_name, 'model_trained.h5')
+    checkpoint = ModelCheckpoint(checkpoint_file, 
+                                 monitor='val_accuracy', 
+                                 save_weights_only=False, 
+                                 verbose=1, 
+                                 save_best_only=True, 
+                                 mode='max')
+    custom_callbacks = [csv_logger, checkpoint]
+
     # ====================================================
     # Build a custom Xception
     # ====================================================
@@ -218,8 +244,8 @@ def main(args):
 
     # compile model
     model.compile(
-        loss=binary_crossentropy,
-        optimizer=Adam(lr=args.lr_pre),
+        loss=binary_crossentropy(from_logits=True),
+        optimizer=Adam(learning_rate=args.lr_pre, episilon=0.1),
         metrics=METRICS
     )
 
@@ -245,7 +271,7 @@ def main(args):
         validation_steps=math.ceil(
             len(val_input_paths) / args.batch_size_pre),
         verbose=1,
-        callbacks=[csv_logger]
+        callbacks=custom_callbacks
     )
 
     # ====================================================
@@ -257,8 +283,8 @@ def main(args):
 
     # recompile
     model.compile(
-        loss=binary_crossentropy,
-        optimizer=Adam(lr=args.lr_fine),
+        loss=binary_crossentropy(from_logits=True),
+        optimizer=Adam(learning_rate=args.lr_fine, epsilon=0.1),
         metrics=METRICS
     )
 
@@ -272,9 +298,8 @@ def main(args):
         validation_steps=math.ceil(
             len(val_input_paths) / args.batch_size_fine),
         verbose=1,
-        callbacks=[csv_logger],
+        callbacks=custom_callbacks
     )
-    model.save(os.path.join(result_path_name, f'model_trained.h5'))
 
     # ====================================================
     # Create & save result graphs
@@ -282,19 +307,18 @@ def main(args):
 
     # Plot the training metrics
     plot_path = os.path.join(result_path_name, 'pre_training_')
-    plot_metrics(hist_fine, plot_path, 'Pre Treino')
+    plot_metrics(hist_pre, plot_path, 'Pré Treino')
 
     plot_path = os.path.join(result_path_name, 'fine_tunning_')
     plot_metrics(hist_fine, plot_path, 'Fine Tunning')
     
 
-    test_dataset = generate_from_paths_and_labels(
+    test_dataset = generate_from_paths(
         input_paths=test_input_paths,
-        labels=test_labels,
         batch_size=args.batch_size_pre
     )
 
-    # train_predictions_baseline = model.predict(train_dataset)
+    # TODO use only the first parameter of the 'test_dataset'
     test_predictions_baseline = model.predict(test_dataset)
 
     # Plot the ROC curve
